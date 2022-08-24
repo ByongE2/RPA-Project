d94f3ba61fac459cb244200651c0826a8898fc8b
ByongE <kbh25963@gmail.com>
2022-08-24 23:28:49 +0900

    ADD Interface of Controllers

---
+++ ProjectRPA/src/main/java/com/rpa/goods/sportswear/controller/Goods_SportswearControllerImpl.java
@@ -0,0 +1,115 @@
+package com.rpa.goods.sportswear.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.rpa.goods.sportswear.domain.AttachImageVO;
+import com.rpa.goods.sportswear.domain.SportswearDto;
+import com.rpa.goods.sportswear.domain.SwCriteria;
+import com.rpa.goods.sportswear.domain.SwPageDto;
+import com.rpa.goods.sportswear.mapper.AttachMapper;
+import com.rpa.goods.sportswear.service.AttachService;
+import com.rpa.goods.sportswear.service.Goods_SportswearService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping
+@AllArgsConstructor
+@Log4j
+public class Goods_SportswearControllerImpl implements Goods_SportswearController {
+
+	private Goods_SportswearService swService;
+	
+	private AttachService attachService;
+	
+	/* 상품 목록 */
+	@Override
+	@GetMapping("/goods/sportswear/list")
+	public String swListGet(SwCriteria cri, Model model) throws Exception{
+		log.info("sw 목록 페이지" + cri);
+		//getList
+		List<SportswearDto> list = swService.swGetList(cri);
+		if(!list.isEmpty()) {
+			model.addAttribute("list",list); //검색결과 존재
+		} else {
+			model.addAttribute("listCheck", "empty");	// 검색결과 존재하지 않을 경우
+		}
+		//페이징
+		int total = swService.swGetTotal(cri);
+		SwPageDto pageMaker = new SwPageDto(cri, total);
+		model.addAttribute("pageMaker", pageMaker);
+		
+		
+		log.info("목록 리스트 정보 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ:" + list);
+		return "Goods/sportswear/swClientList";
+	}//swGetList
+	
+	
+	/* 상품 상세 정보*/
+	@Override
+	@GetMapping("/goods/sportswear/detail/{goods_swID}")
+	public String swDetailGet(@PathVariable("goods_swID")Long swID, Model model) {
+		
+		log.info("상품 사용자 상세 페이지..........");
+		
+		model.addAttribute("swInfo", swService.swGetDetail(swID));
+		
+		return "/Goods/sportswear/swClientDetail";
+	}
+	
+	
+	/* 이미지 출력 */
+	@Override
+	@GetMapping("/goods/sportswear/display")
+	//이미지 파일은 바이너리 파일 범주임(text파일을 제외하면 다 바이너리 파일 0,1 범주임) 그래서 byte배열 byte[] 사용
+	public ResponseEntity<byte[]> getSwImage(String fileName){
+		log.info("getImage()ㅡㅡㅡㅡㅡㅡㅡ:" + fileName);
+		
+		//'유동 경로' + '파일 이름'
+		File file = new File("c:\\upload\\" + fileName);
+
+		ResponseEntity<byte[]> result = null;
+		try {
+			//Response의 header와 관련된 설정의 객체를 추가해주기 위해서
+			HttpHeaders header = new HttpHeaders();
+			//'Content Type' 속성 값에 이미지 파일 MIME TYPE을 추가해주기 위해서 HttpHeader 클래스에 있는 add() 메서드를 사용
+			header.add("Content-type", Files.probeContentType(file.toPath()));//  Response header의 '속성명', '속성명'에 부여할 값
+			//FileCopyUtils 클래스는 파일과 stream 복사에 사용할 수 있는 메서드를 제공하는 클래스
+			//copyToByteArray() 메서드는 파라미터로 부여하는 File 객체 즉, 대상 파일을 복사하여 byte 배열로 반환해주는 클래스
+			result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+		}catch (IOException e) {
+			e.printStackTrace();
+		}//end of try-catch
+		
+		return result;
+	}//getSwImage
+	
+	
+	//상세페이지 넘어갈 때 상품+ 상품이미지 정보 같이 넘겨주는 방법도 있으나,
+	//상세페이지 이동 후 AJAX 또는 getJSON으로 정보 요청하는 방법으로 하겠음.(여기선 getJSON)
+	/* 이미지 정보 반환 */
+	@Override
+	@GetMapping(value="/getAttachList", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<AttachImageVO>> getAttachList(Long swID){
+		
+		log.info("getAttachList..........swID : " + swID);
+		
+		return new ResponseEntity<List<AttachImageVO>>(attachService.getAttachList(swID), HttpStatus.OK);
+	}//getAttachList
+	
+}//Goods_SportswearController
--- ProjectRPA/src/main/java/com/rpa/user/controller/UserController.java
+++ ProjectRPA/src/main/java/com/rpa/user/controller/UserController.java
@@ -1,184 +1,34 @@
 package com.rpa.user.controller;
 
-import java.util.Random;
-
-import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.rpa.user.domain.UserDTO;
-import com.rpa.user.service.UserService;
 
-import lombok.AllArgsConstructor;
-import lombok.extern.log4j.Log4j;
+public interface UserController {
 
-@Controller
-@RequestMapping("/user")
-@Log4j
-public class UserController {
-	
-	@Autowired
-	private UserService userService;
-	@Autowired
-	private JavaMailSender mailSender;
-	/*@Autowired로는 빈 등록해도 빈을 찾을 수 없다는 에러가남..*/
-	@Bean
-	BCryptPasswordEncoder pwEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-	
 	/* 회원가입 */
-	@GetMapping("/join")
-	public void joinGET() {
-		log.info("회원가입 페이지 진입 GETㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
-	}//joinGET
-	
-	@PostMapping("/join")
-	public String joinPOST(UserDTO user) throws Exception{
-		
-		log.info("회원가입 페이지 진입 POSTㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
-		
-		userService.userJoin(user);
-		
-		return "redirect:/main";
-	}//joinPOST
-	
-	/* 아이디 중복 검사 */
-	@PostMapping("/userIdChk")
-	@ResponseBody
-	public String userIDCheckPOST(String id) throws Exception{
-		
-		int result = userService.userIDCheck(id);
-		
-		if(result != 0) {
-			return "fail";	// 중복 아이디가 존재
-		} else {
-			return "success";	// 중복 아이디 x
-		}		
-	} // userIDCheckPOST()
-	
-	/* 이메일 인증 */
-	@GetMapping("/mailCheck")
-	@ResponseBody
-	public String mailCheckGET(String email) throws Exception{
-		
-		/* 뷰(View)로부터 넘어온 데이터 확인 */
-		log.info("이메일 데이터 전송 확인");
-		log.info("이메일 : " + email);
-				
-		/* 인증번호(난수) 생성 */
-		Random random = new Random();
-		int checkNum = random.nextInt(888888) + 111111;
-		log.info("인증번호 " + checkNum);
-		
-		/* 이메일 보내기 */
-		String setFrom = "kbh25962@naver.com";  //root-context의 관리자 이메일
-		String toMail = email;
-		String title = "RPA Fitness 회원가입 인증 이메일 입니다.";
-		String content = 
-				"Welcome RPA Fitness" +
-				"<br><br>" + 
-				"인증 번호는 " + checkNum + "입니다." + 
-				"<br>" + 
-				"해당 인증번호를 인증번호 확인란에 기입하여 주세요.";		
-		try {
-			/*이메일 전송 코드*/
-			MimeMessage message = mailSender.createMimeMessage();
-			MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
-			helper.setFrom(setFrom);
-			helper.setTo(toMail);
-			helper.setSubject(title);
-			helper.setText(content,true);
-			mailSender.send(message);
-			
-		}catch(Exception e) {
-			e.printStackTrace();
-		}		
-		
-		String num = Integer.toString(checkNum);
-		
-		return num;
-	}//mailCheckGET
-	
-	/* 로그인 페이지 */
-	@GetMapping("/login")
-	public void loginGET() {
-		
-		log.info("로그인 페이지 진입ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
-		
-	}//loginGET
-	
-	@PostMapping("/login")
-	public String loginPOST(HttpServletRequest request, UserDTO user, RedirectAttributes rttr) throws Exception{
-		
-		HttpSession session = request.getSession();
-		String rawPw = "";		//인코딩 전 비밀번호
-		String encodePw = "";	//인코딩 후 비밀번호
-	
-		UserDTO loginDto = userService.userLogin(user);	// 입력받은 아이디 일치하는 지 체크(id만 먼저)
-		
-		/*id 일치여부 체크하고 > 인코딩된 비밀번호 체크 순서*/
-		if(loginDto != null) {										// 일치하는 아이디 존재시
-			
-			rawPw = user.getPw();				// 사용자가 제출한 비밀번호 
-			encodePw = loginDto.getPw();		// 데이터베이스에 저장된 인코딩된 비밀번호 
-					
-					//pwEncoder : BCryptPasswordEncoder 타입
-			if(true == pwEncoder().matches(rawPw, encodePw)) {		// 비밀번호 일치하면,
-				
-				loginDto.setPw("");							// loginDto에서 인코딩된 비밀번호 정보 지움
-				session.setAttribute("user", loginDto); 	// session에 사용자의 정보 저장
+	void joinGET();//joinGET
+	String joinPOST(UserDTO user) throws Exception;//joinPOST
 
-				return "redirect:/main";		// 메인페이지 이동
-			} else {												// 비밀번호 불일치하면,
-				rttr.addFlashAttribute("result", 0);//0:실패 1:성공			
-				return "redirect:/user/login";	// 로그인 페이지로 이동
-			}
-		} else {													// 일치하는 아이디가 존재하지 않을 시 (로그인 실패)
-			rttr.addFlashAttribute("result", 0);			
-			return "redirect:/user/login";	// 로그인 페이지로 이동
-		}
-	}//loginPOST
-	
-    /* 메인페이지 로그아웃 */
-    @GetMapping("/logout")
-    public String logoutMainGET(HttpServletRequest request) throws Exception{
-        
-        log.info("로그아웃 메서드(logoutMainGET) 실행 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
-        
-        HttpSession session = request.getSession();
-        
-        session.invalidate();
-        
-        return "redirect:/main";        
-    }//logoutMainGET
-	
+	/* 아이디 중복 검사 */
+	String userIDCheckPOST(String id) throws Exception; // userIDCheckPOST()
+
+	/* 이메일 인증 */
+	String mailCheckGET(String email) throws Exception;//mailCheckGET
+
+	/* 로그인 페이지 */
+	void loginGET();//loginGET
+	String loginPOST(HttpServletRequest request, UserDTO user, RedirectAttributes rttr) throws Exception;//loginPOST
+
+	/* 메인페이지 로그아웃 */
+	String logoutMainGET(HttpServletRequest request) throws Exception;//logoutMainGET
+
 	/* 비동기방식 로그아웃 메서드 */
-    @PostMapping("/logout")
-    @ResponseBody
-    public void logoutPOST(HttpServletRequest request) throws Exception{
-    	
-    	log.info("비동기 로그아웃 메서드 진입");
-    	
-    	HttpSession session = request.getSession();
-    	
-    	session.invalidate();
-    }//logoutPOST
-	
-}//UserController
+	void logoutPOST(HttpServletRequest request) throws Exception;//logoutPOST
+
+}
